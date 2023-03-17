27731ced2ff10dfbda4297b00f14bc5dd8669a83
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-17 15:51:25 +0530

    Create CO23AD0601_10_AA1.py

---
+++ CO23AD0601_10_AA1.py
@@ -0,0 +1,11 @@
+fahrenheit_value_1= 32
+celsius_value_1 = ((fahrenheit_value_1-32)*5)/9  
+print("The Celsius equivalent of",fahrenheit_value_1,"degree Fahrenheit is :", celsius_value_1);  
+
+fahrenheit_value_2= 50
+celsius_value_2 = ((fahrenheit_value_2-32)*5)/9  
+print("The Celsius equivalent of",fahrenheit_value_2,"degree Fahrenheit is :", celsius_value_2);  
+
+fahrenheit_value_3= 212
+celsius_value_3 = ((fahrenheit_value_3-32)*5)/9  
+print("The Celsius equivalent of",fahrenheit_value_3,"degree Fahrenheit is :", celsius_value_3);    
